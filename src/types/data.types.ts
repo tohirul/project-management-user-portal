23b53c65a1d27a7c99a53bb0100e3bb659d9ecfb
tohirul <tohirul.islam164@gmail.com>
2025-08-18 19:03:40 +0600

    Module Types creatd

---
+++ src/types/data.types.ts
@@ -0,0 +1,55 @@
+import { Priority, Status } from "@/constants/enum";
+
+export interface Project {
+  id: number;
+  name: string;
+  description?: string;
+  startDate: string;
+  endDate?: string;
+}
+
+export interface User {
+  userId: number;
+  username: string;
+  email: string;
+  profilePictureUrl?: string;
+  congnitoId?: string;
+  teamId?: number;
+}
+
+export interface Comment {
+  id: string;
+  content: string;
+  authorId: string;
+  postId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Attachment {
+  id: number;
+  fileUrl: string;
+  filename: string;
+  taskId: number;
+  uploadedById: number;
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string[];
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
+  projectId: number;
+  authorUserId: number;
+  assignedUserId?: number;
+
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[];
+}
