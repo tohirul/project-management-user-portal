a955c6524836ac67de4c83c4200ddad4cd56d63c
tohirul <tohirul.islam164@gmail.com>
2025-08-18 19:03:00 +0600

    Module Constants

---
+++ src/constants/enum.ts
@@ -0,0 +1,14 @@
+export enum Status {
+  ToDO = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
+
+export enum Priority {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
