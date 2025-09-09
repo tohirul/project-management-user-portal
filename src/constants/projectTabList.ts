e8f14412110fa4631a2dec66e453bf38b9e82846
tohirul <tohirul.islam164@gmail.com>
2025-09-09 23:19:41 +0600

    Up to Date

---
+++ src/constants/projectTabList.ts
@@ -0,0 +1,9 @@
+import { Clock, Grid3X3, List, Table2, ViewIcon } from "lucide-react";
+
+export const projectTablist = [
+  { name: "Overview", icon: ViewIcon },
+  { name: "Board", icon: Grid3X3 },
+  { name: "List", icon: List },
+  { name: "Timeline", icon: Clock },
+  { name: "Table", icon: Table2 },
+];
